1106afaf882efb0f6e0ef6cdb90d1e957e6937ae
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-01-18 14:55:31 -0900

    add: hw1 (initial files)

---
+++ checkers/game.py
@@ -0,0 +1,12 @@
+# game.py
+# Solomon Himelbloom
+# 2022-01-18
+#
+# For CS 405 Spring 2022
+"""Basic checkers implementation."""
+
+import math
+import random
+import time
+
+print("Hello, checkers.")
